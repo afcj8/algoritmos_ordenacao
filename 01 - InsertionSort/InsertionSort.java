8b2b47c83212c8630423f232432a49b98e8275cb
Antônio Junior <102259875+afcj8@users.noreply.github.com>
2024-11-25 16:52:30 +0000

    teste do método insertionSort

--- 01 - InsertionSort/InsertionSort.java
+++ 01 - InsertionSort/InsertionSort.java
@@ -1,6 +1,19 @@
 public class InsertionSort {
     public static void main(String[] args) {
+        int quantidade = 1000;
+        int[] arr = new int[quantidade];
 
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 1000);
+        }
+
+        long tempoInicial = System.currentTimeMillis();
+
+        insertionSort(arr);
+
+        long tempoFinal = System.currentTimeMillis();
+
+        System.out.println("Executado em = " + (tempoFinal - tempoInicial) + " ms");
     }
 
 
